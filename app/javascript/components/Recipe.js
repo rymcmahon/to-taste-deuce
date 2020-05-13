6053b2814da09ede8d245f6032e25957363465f4
rymcmahon <toedragdangle9@gmail.com>
2020-05-12 22:10:41 -0400

    added ingredients map for recipe

--- app/javascript/components/Recipe.js
+++ app/javascript/components/Recipe.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import PropTypes from "prop-types";
 
 function Recipe(props) {
-  const [recipe, setRecipe] = useState([]);
-
+  const [recipe, setRecipe] = useState({});
+  const [ingredients, setIngredients] = useState([]);
   useEffect(() => {
     axios
       .get(`http://localhost:3000/api/v1/recipes/${props.recipeId}.json`)
       .then(function (response) {
         setRecipe(response.data);
+        setIngredients(response.data.ingredients);
       })
       .catch(function (error) {
         console.log(error);
@@ -18,10 +19,19 @@ function Recipe(props) {
 
   return (
     <div>
-      <p>{recipe.name}</p>
+      <h1>{recipe.name}</h1>
       <p>{recipe.description}</p>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.id}>{ingredient.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+Recipe.propTypes = {
+  recipeId: PropTypes.string,
+};
 
 export default Recipe;
