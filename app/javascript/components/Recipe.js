ba9ab1e2d1d258fa819c7217c5187d959e3ec5b9
rymcmahon <toedragdangle9@gmail.com>
2020-05-12 20:40:31 -0400

    deleted pages controller and added recipes controller

--- app/javascript/components/Recipe.js
+++ app/javascript/components/Recipe.js
@@ -2,13 +2,26 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 
-function Recipe() {
+function Recipe(props) {
   const [recipe, setRecipe] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/v1/recipes/`${id}`.json");
+    axios
+      .get(`http://localhost:3000/api/v1/recipes/${props.recipeId}.json`)
+      .then(function (response) {
+        setRecipe(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }, []);
-  return <div></div>;
+
+  return (
+    <div>
+      <p>{recipe.name}</p>
+      <p>{recipe.description}</p>
+    </div>
+  );
 }
 
 export default Recipe;
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  resources :recipes
+
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index, :show]
