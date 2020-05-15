cd18eea99a47352dea5e4744aea90433e7a3ed5d
rymcmahon <toedragdangle9@gmail.com>
2020-05-15 09:35:29 -0400

    added controller code for creating recipe and ingredients

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -10,8 +10,18 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def create
-    puts '********************************'
-    puts params["recipe"]
-    puts '********************************'
+    @recipe = Recipe.create(
+      name: params["recipe"]["name"],
+      description: params["recipe"]["description"],
+      cooking_time: params["recipe"]["cookingTime"],
+      prep_time: params["recipe"]["prepTime"],
+      yield: params["recipe"]["yield"]
+    )
+
+    @ingredients = params["ingredients"]
+    # @ingredients.each do |ingredient|
+    #   @recipe.ingredients.create(name: ingredient["name"])
+    # end
+    @ingredients.map{ |ingredient| @recipe.ingredients.create(name: ingredient["name"]) }
   end
 end
