2d61fcbcf9b9effcf2b8065176544e9cba705f07
rymcmahon <toedragdangle9@gmail.com>
2020-06-09 09:06:46 -0400

    added carrierwave gem

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :instructions
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_002249) do
+ActiveRecord::Schema.define(version: 2020_06_09_123835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_002249) do
     t.integer "yield"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   add_foreign_key "ingredients", "recipes"
