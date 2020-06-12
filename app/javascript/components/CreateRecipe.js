5fb9ef200f0cf7a62804a1a1306c2cbba2d4a4d9
rymcmahon <toedragdangle9@gmail.com>
2020-06-12 08:52:35 -0400

    direct upload

--- app/javascript/components/CreateRecipe.js
+++ app/javascript/components/CreateRecipe.js
@@ -8,6 +8,7 @@ import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import SaveIcon from "@material-ui/icons/Save";
 import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
 import IconButton from "@material-ui/core/IconButton";
+import * as ActiveStorage from "activestorage";
 import Input from "@material-ui/core/Input";
 
 // {
@@ -18,6 +19,51 @@ import Input from "@material-ui/core/Input";
 //     },
 //   },
 // }
+
+addEventListener("direct-upload:initialize", (event) => {
+  const { target, detail } = event;
+  const { id, file } = detail;
+  target.insertAdjacentHTML(
+    "beforebegin",
+    `
+    <div id="direct-upload-${id}" class="direct-upload direct-upload--pending">
+      <div id="direct-upload-progress-${id}" class="direct-upload__progress" style="width: 0%"></div>
+      <span class="direct-upload__filename"></span>
+    </div>
+  `
+  );
+  target.previousElementSibling.querySelector(
+    `.direct-upload__filename`
+  ).textContent = file.name;
+});
+
+addEventListener("direct-upload:start", (event) => {
+  const { id } = event.detail;
+  const element = document.getElementById(`direct-upload-${id}`);
+  element.classList.remove("direct-upload--pending");
+});
+
+addEventListener("direct-upload:progress", (event) => {
+  const { id, progress } = event.detail;
+  const progressElement = document.getElementById(
+    `direct-upload-progress-${id}`
+  );
+  progressElement.style.width = `${progress}%`;
+});
+
+addEventListener("direct-upload:error", (event) => {
+  event.preventDefault();
+  const { id, error } = event.detail;
+  const element = document.getElementById(`direct-upload-${id}`);
+  element.classList.add("direct-upload--error");
+  element.setAttribute("title", error);
+});
+
+addEventListener("direct-upload:end", (event) => {
+  const { id } = event.detail;
+  const element = document.getElementById(`direct-upload-${id}`);
+  element.classList.add("direct-upload--complete");
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -147,7 +193,11 @@ function CreateRecipe() {
               type="file"
               id="image"
               name="image"
-              onChange={handleRecipeChange}
+              inputProps={{
+                "data-direct-upload-url":
+                  "https://localhost:3000/rails/active_storage/direct_uploads",
+              }}
+              // onChange={handleRecipeChange}
               variant="outlined"
               fullWidth
             />
